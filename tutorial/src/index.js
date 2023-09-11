9ad3b03e9db6f4976a8843000337680f08cf9eb9
Falalatina <karolina40.gorska17@gmail.com>
2023-09-11 12:07:46 +0200

    css in react

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,21 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greetings() {
-  return (
-    <React.Fragment>
-      <Person />
-      <Message />
-    </React.Fragment>
-  );
-  // return React.createElement("h2", {}, "hello");
-}
+import "./index.css";
 
-const Person = () => <h1>John doe</h1>;
-const Message = () => {
-  return <p>this is message</p>;
+const BookList = () => {
+  return (
+    <section className="bookList">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+//1:47
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg"
+    alt="Holly"
+  />
+);
+const Title = () => <h2>Holly</h2>;
+const Author = () => {
+  return <h4>Stephen King</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greetings />);
+root.render(<BookList />);
