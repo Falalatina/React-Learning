428ce3ee2162f246ef32da48a0f988be8bc2d31c
Falalatina <karolina40.gorska17@gmail.com>
2023-09-12 12:53:46 +0200

    add different books

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -6,17 +6,42 @@ import "./index.css";
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book />
-      <Book author={author} title={title} img={img} />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
     </section>
   );
 };
 
-const author = "Stephen King";
-const title = "Holly";
-const img =
-  "https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg";
+const firstBook = {
+  author: "Stephen King",
+  title: "Holly",
+  img: "https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg",
+};
+
+const secondBook = {
+  author: "Arthur C. Brooks & Oprah Winfrey",
+  title: "Build the Life You Want",
+  img: "./images/book-2.jpg",
+};
+
+const thirdBook = {
+  author: "Keila Shaheen",
+  title: "The Shadow Work Journal",
+  img: "./images/book-3.jpg",
+};
 
 const Book = (props) => {
   return (
