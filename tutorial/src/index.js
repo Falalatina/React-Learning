17d5f5d5fc78ef1d8a5732378cc56e390700b5c9
Falalatina <karolina40.gorska17@gmail.com>
2023-09-14 13:55:43 +0200

    searching by id

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -25,28 +25,30 @@ const books = [
 ];
 
 const BookList = () => {
-  const someValue = "shakeAndBake";
-  const displeyValue = () => {
-    console.log(someValue);
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book.title);
   };
 
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displeyValue={displeyValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, children, displeyValue } = props;
-
+  const { img, title, author, children, getBook, id } = props;
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displeyValue}>Enjoy</button>
+      <button onClick={getSingleBook}>Enjoy</button>
       <h4>{author}</h4>
       {children}
     </article>
