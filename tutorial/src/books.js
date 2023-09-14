6fce46c5b0d581d64285c34f0bb8468860f1651a
Falalatina <karolina40.gorska17@gmail.com>
2023-09-14 14:16:33 +0200

    ES6 module

---
+++ tutorial/src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    author: "Stephen King",
+    title: "Holly",
+    img: "https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg",
+    id: 1,
+  },
+  {
+    author: "Arthur C. Brooks & Oprah Winfrey",
+    title: "Build the Life You Want",
+    img: "./images/book-2.jpg",
+    id: 2,
+  },
+  {
+    author: "Keila Shaheen",
+    title: "The Shadow Work Journal",
+    img: "./images/book-3.jpg",
+    id: 3,
+  },
+];
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,27 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const books = [
-  {
-    author: "Stephen King",
-    title: "Holly",
-    img: "https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg",
-    id: 1,
-  },
-  {
-    author: "Arthur C. Brooks & Oprah Winfrey",
-    title: "Build the Life You Want",
-    img: "./images/book-2.jpg",
-    id: 2,
-  },
-  {
-    author: "Keila Shaheen",
-    title: "The Shadow Work Journal",
-    img: "./images/book-3.jpg",
-    id: 3,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
@@ -31,18 +12,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, children } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      {children}
-    </article>
   );
 };
 
