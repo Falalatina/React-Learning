80e04fb253a572f37fec5ad92afd71c654bc33a1
Falalatina <karolina40.gorska17@gmail.com>
2023-09-14 15:03:44 +0200

    local img

--- tutorial/src/books.js
+++ tutorial/src/books.js
@@ -1,20 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
 export const books = [
   {
     author: "Stephen King",
     title: "Holly",
-    img: "https://m.media-amazon.com/images/I/417kSOgt4aL._SX325_BO1,204,203,200_.jpg",
+    img: img1,
     id: 1,
   },
   {
     author: "Arthur C. Brooks & Oprah Winfrey",
     title: "Build the Life You Want",
-    img: "./images/book-2.jpg",
+    img: img2,
     id: 2,
   },
   {
     author: "Keila Shaheen",
     title: "The Shadow Work Journal",
-    img: "./images/book-3.jpg",
+    img: img3,
     id: 3,
   },
 ];
