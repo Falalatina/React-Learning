ffe063d634b96c89fd993a6668855e041b31f803
Falalatina <karolina40.gorska17@gmail.com>
2023-09-21 12:35:49 +0200

    Update 05-useState-gotcha.jsx

--- my-react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ my-react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,10 +3,22 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
   const updateVal = () => {
-    setValue(value + 1);
+    setTimeout(() => {
+      setValue((currentState) => {
+        const newState = currentState + 1;
+        return newState;
+      });
+    }, 3000);
   };
 
-  return <h2>Val is: {value}</h2>;
+  return (
+    <>
+      <h2>Val is: {value}</h2>
+      <button type="button" className="btn" onClick={updateVal}>
+        Add
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
