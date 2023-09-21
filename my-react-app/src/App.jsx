bcbf210447a008a35ec78070ddd4d6d4edcd4774
Falalatina <karolina40.gorska17@gmail.com>
2023-09-21 12:50:55 +0200

    useEffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { data } from "./data";
 
-import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
+import Starter from "./tutorial/02-useEffect/starter/01-code-example.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
