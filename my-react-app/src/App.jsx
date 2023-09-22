87b6d978829287d32e5adb6c38704fc3f1c24eaf
Falalatina <karolina40.gorska17@gmail.com>
2023-09-22 15:34:36 +0200

    truthy falsy

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { data } from "./data";
 
-import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule";
+import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
 
 function App() {
   const [count, setCount] = useState(0);
--- my-react-app/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ my-react-app/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  //falsy
+  const [text, setText] = useState("");
+  //truthy
+  const [name, setName] = useState("Susan");
+
+  let btnText = " Text is true";
+  return (
+    <div>
+      <h4>Falsy Or: {text || "text is falsy"}</h4>
+      <h4>Falsy And: {text && "text is truthy"}</h4>
+      <h4>Truthy Or: {name || "text is falsy"}</h4>
+      <h4>Truthy And: {name && "text is truthy"}</h4>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setText("im truthy");
+        }}
+      >
+        {btnText}
+      </button>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
