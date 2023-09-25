74ca433853048e9ada1acd953369dd34a498fdbb
Falalatina <karolina40.gorska17@gmail.com>
2023-09-25 15:11:11 +0200

    add handle submin

--- my-react-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ my-react-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -2,22 +2,45 @@ import { useState } from "react";
 import { data } from "../../../data";
 
 const UserChallenge = () => {
+  const [name, setName] = useState("");
   const [list, setList] = useState(data);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    const fakeId = Date.now();
+    const newUser = { id: fakeId, name };
+    const updateList = [...list, newUser];
+    setList(updateList);
+
+    setName("");
+  };
+
+  const handleName = (e) => {
+    //console.log(e.target.value);
+    setName(e.target.value);
+  };
+
   const deletePerson = (id) => {
-    const newList = list.filter((person) => person.id !== id);
-    setList(newList);
+    const updateList = list.filter((person) => person.id !== id);
+    setList(updateList);
   };
 
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className="form-row">
           <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type="text" className="form-input" id="name" />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={handleName}
+          />
         </div>
 
         <button type="submit" className="btn btn-block">
