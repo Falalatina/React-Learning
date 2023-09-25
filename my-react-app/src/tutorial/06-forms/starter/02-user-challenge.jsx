77002aefb9a0d9bbaf76369575877873fd16810a
Falalatina <karolina40.gorska17@gmail.com>
2023-09-25 14:35:54 +0200

    add delete btn

--- my-react-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ my-react-app/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,53 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [list, setList] = useState(data);
+
+  const deletePerson = (id) => {
+    const newList = list.filter((person) => person.id !== id);
+    setList(newList);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type="text" className="form-input" id="name" />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      {list.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <h3>{name}</h3>
+              <button
+                type="button"
+                className="btn "
+                style={{ margin: "1rem" }}
+                onClick={() => deletePerson(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
