5aa64d00c024a7597043e16daf4d437dccb3635f
Falalatina <karolina40.gorska17@gmail.com>
2023-09-25 11:22:16 +0200

    folder structure

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 
 import { data } from "./data";
 
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+
+// import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="container">
-      <Starter key={data.id} />
+      <Navbar />
     </div>
   );
 }
