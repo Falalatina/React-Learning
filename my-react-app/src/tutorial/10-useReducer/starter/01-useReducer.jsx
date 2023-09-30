1478876d9d60b6b592f1c566b484fd45285ee403
Falalatina <karolina40.gorska17@gmail.com>
2023-09-30 16:46:34 +0200

    Update 01-useReducer.jsx

--- my-react-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ my-react-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
+import { useState } from "react";
 const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
+  const [isTherePeople, setIsTherePeople] = useState(true);
+
+  const restoreList = () => {
+    setPeople(data);
+    setIsTherePeople(true);
+  };
 
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
+  };
+
+  const deleteAll = () => {
+    setPeople([]);
+    setIsTherePeople(false);
   };
   return (
     <div>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {isTherePeople ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={deleteAll}
+        >
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={restoreList}
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 };
