1468263d07fe6671b68412dfda81d2ac1abe467f
Falalatina <karolina40.gorska17@gmail.com>
2023-10-05 13:10:30 +0200

    add delete item and reset func

--- my-react-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ my-react-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -15,16 +15,15 @@ const reducer = (state, action) => {
   if (action.type === CLEAR_LIST) {
     return { ...state, people: [] };
   }
+  if (action.type === RESTORE_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_ITEM) {
+    const people = state.people;
+    let newPeople = people.filter((person) => person.id !== action.payload.id);
+    return { ...state, people: newPeople };
+  }
   throw new Error(`No matching "${action.type}" - action type `);
-
-  // if (action.type === RESTORE_LIST) {
-  //   return { ...state, people: data };
-  // }
-  // if (action.type === REMOVE_ITEM) {
-  //   const people = state.people;
-  //   let newPeople = people.filter((person) => person.id !== id);
-  //   return { ...state, people: newPeople };
-  //}
 };
 
 const ReducerBasics = () => {
@@ -37,7 +36,7 @@ const ReducerBasics = () => {
   };
 
   const removeItem = (id) => {
-    dispatch({ type: REMOVE_ITEM });
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
     // setPeople(newPeople);
   };
 
