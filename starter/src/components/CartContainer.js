ad1df5604953ae7c4b78e3280f85b11ac17470eb
Falalatina <karolina40.gorska17@gmail.com>
2023-11-28 13:59:53 +0100

    add cart

--- starter/src/components/CartContainer.js
+++ starter/src/components/CartContainer.js
@@ -1,7 +1,20 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 const CartContainer = () => {
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your cart</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
   return <div>CartContainer</div>;
 };
 
