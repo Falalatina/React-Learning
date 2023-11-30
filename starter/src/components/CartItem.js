3934ef630abaaef69e808bd418c5b53bfe1c1a69
Falalatina <karolina40.gorska17@gmail.com>
2023-11-30 14:01:35 +0100

    Update CartItem.js

--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ChevronDown, ChevronUp } from "../icons";
 import { useDispatch } from "react-redux";
 import { clearItem } from "../features/cart/cartSlice";
+
 const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
   return (
