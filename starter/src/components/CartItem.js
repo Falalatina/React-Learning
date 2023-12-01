925047267e4ef3c50a51206e4af0418593f4fc15
Falalatina <karolina40.gorska17@gmail.com>
2023-12-01 14:34:40 +0100

    Update CartItem.js

--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -18,14 +18,19 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         <button
           className="amount-btn"
-          onClick={() => dispatch(increase({ id, amount }))}
+          onClick={() => dispatch(increase({ id }))}
         >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
         <button
           className="amount-btn"
-          onClick={() => dispatch(decrease({ id, amount }))}
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(clearItem(id));
+            }
+            dispatch(decrease({ id }));
+          }}
         >
           <ChevronDown />
         </button>
