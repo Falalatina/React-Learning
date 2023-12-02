3dbf7654e9bb63cab03dca391c4c07448e9b24a7
Falalatina <karolina40.gorska17@gmail.com>
2023-12-02 13:06:53 +0100

    add loading

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,17 +1,32 @@
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotal } from "./features/cart/cartSlice";
+import { calculateTotal, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import Modal from "./components/Modal";
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
   const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotal());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="loading">
+          <h1>Loading...</h1>
+        </div>
+      </>
+    );
+  }
+
   return (
     <main>
       {isOpen && <Modal />}
