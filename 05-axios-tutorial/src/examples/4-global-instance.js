826983145dc8ba85f361be9acd59e2012cc53f71
Falalatina <karolina40.gorska17@gmail.com>
2023-12-04 11:53:39 +0100

    global instance

--- 05-axios-tutorial/src/examples/4-global-instance.js
+++ 05-axios-tutorial/src/examples/4-global-instance.js
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 
-const productsUrl = 'https://course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = "https://course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const { data } = await axios(productsUrl);
+      const resp = await axios(randomUserUrl);
+      console.log(data);
+      console.log(resp);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
